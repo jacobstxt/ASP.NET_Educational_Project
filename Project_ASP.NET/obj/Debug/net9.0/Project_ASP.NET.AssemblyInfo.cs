d6dfd0101e89d8bd791788e125ccf7b812c52c9c
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-04-22 16:16:32 +0300

    Fix

--- Project_ASP.NET/obj/Debug/net9.0/Project_ASP.NET.AssemblyInfo.cs
+++ Project_ASP.NET/obj/Debug/net9.0/Project_ASP.NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_ASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3710b354e8506084203958db9fbe02dbc5f3a3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5affa572c3b4ba0441828e2014c91d1234e1fd08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_ASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_ASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
